d64f1688b6687f44c97ff31aee799989fb1ea539
Nikita <mordnikita17@gmail.com>
2026-09-04 15:06:14 +0500

    New base url not mixed, deleted .env

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "01492ed8adf6804b73e45cab4dd6b513
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "162d3dbf5fe0f58ef7c87422d347c544",
+"flutter_bootstrap.js": "51ceac7a92d7b00147261eadc316d8e6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
